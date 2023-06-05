8fd4e2f1f00ae905db6972621fd6b0d741956aa0
Jacob Seymour <jacobwseymour@gmail.com>
2023-06-05 19:31:02 +0100

    added files, started work on JS append buttons and added sounds

--- Sound Board/script.js
+++ Sound Board/script.js
@@ -1,1 +1,25 @@
+const sounds = ['applause', 'jump', 'impact', 'wrong', 'victory', 'defeat', 'noti', 'boo', 'swoosh', 'sword']
 
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
